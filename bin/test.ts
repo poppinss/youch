de74a5af6f7d8937f00a1a11f260488cda95f620
Harminder Virk <virk.officials@gmail.com>
2025-01-17 10:16:40 +0530

    test: fix breaking tests

--- bin/test.ts
+++ bin/test.ts
@@ -2,12 +2,13 @@ import { expect } from '@japa/expect'
 import { assert } from '@japa/assert'
 import { fileSystem } from '@japa/file-system'
 import { expectTypeOf } from '@japa/expect-type'
+import { snapshot } from '@japa/snapshot'
 import { configure, processCLIArgs, run } from '@japa/runner'
 
 processCLIArgs(process.argv.splice(2))
 configure({
   files: ['tests/**/*.spec.ts'],
-  plugins: [expect(), assert(), fileSystem(), expectTypeOf()],
+  plugins: [expect(), assert(), fileSystem(), expectTypeOf(), snapshot()],
 })
 
 run()
--- tests/templates.spec.ts
+++ tests/templates.spec.ts
@@ -31,58 +31,85 @@ test.group('Templates', () => {
     expect(window.document.querySelector('#error-message')?.textContent?.trim()).toEqual(
       'Something went wrong'
     )
-    expect(window.document.querySelector('#header-script')?.textContent?.trim()).toEqual(
-      expect.stringContaining(
-        'document\n' +
-          '  .getElementById("toggle-theme-checkbox")\n' +
-          '  ?.addEventListener(\n' +
-          "    \'change\',\n" +
-          '    function() {\n' +
-          '      toggleTheme(this)\n' +
-          '    }\n' +
-          ')'
-      )
-    )
-    expect(window.document.querySelector('#errorStack-script')?.textContent?.trim()).toEqual(
-      expect.stringContaining(
-        'document\n' +
-          "  .getElementById('formatted-frames')\n" +
-          '  ?.addEventListener(\n' +
-          "    'click',\n" +
-          '    function(){\n' +
-          '      showFormattedFrames(this)\n' +
-          '    }\n' +
-          '  )\n' +
-          'document\n' +
-          "  .getElementById('raw-frames')\n" +
-          '  ?.addEventListener(\n' +
-          "    'click',\n" +
-          '    function(){\n' +
-          '      showRawFrames(this)\n' +
-          '    }\n' +
-          '  )\n' +
-          'document\n' +
-          `  .querySelectorAll("[id^='stack-frame-location-']")\n` +
-          '  .forEach((sfl) => {\n' +
-          '    sfl.addEventListener(\n' +
-          "      'click',\n" +
-          '      function(e){\n' +
-          "        toggleFrameSource(e, this.id.replace('stack-frame-location-', 'frame-'))\n" +
-          '      }\n' +
-          '    )\n' +
-          '  })\n' +
-          'document\n' +
-          `  .querySelectorAll("[id^='stack-frame-toggle-indicator-']")\n` +
-          '  .forEach((sfti) => {\n' +
-          '    sfti.addEventListener(\n' +
-          "      'click',\n" +
-          '      function(e){\n' +
-          "        toggleFrameSource(e, this.id.replace('stack-frame-toggle-indicator-', 'frame-'))\n" +
-          '      }\n' +
-          '    )\n' +
-          '  })'
-      )
-    )
+    expect(window.document.querySelector('#header-script')?.textContent?.trim())
+      .toMatchInlineSnapshot(`
+      "function toggleTheme(input) {
+        if (input.checked) {
+          document.documentElement.classList.add('dark')
+          localStorage.setItem('youch-theme', 'dark')
+        } else {
+          document.documentElement.classList.remove('dark')
+          localStorage.setItem('youch-theme', 'light')
+        }
+      }
+
+      window.addEventListener('DOMContentLoaded', () => {
+        document.querySelector('#toggle-theme-checkbox').checked = usesDarkMode()
+        document.querySelector('#toggle-theme-checkbox').addEventListener('change', function () {
+          toggleTheme(this)
+        })
+      })"
+    `)
+    expect(window.document.querySelector('#errorStack-script')?.textContent?.trim())
+      .toMatchInlineSnapshot(`
+      "function showFormattedFrames(button) {
+        const parent = button.closest('section')
+
+        const formattedFrames = parent.querySelector('#stack-frames-formatted')
+        formattedFrames.classList.add('visible')
+
+        const rawFrames = parent.querySelector('#stack-frames-raw')
+        rawFrames.classList.remove('visible')
+
+        button.parentElement.querySelectorAll('button').forEach((btn) => btn.classList.remove('active'))
+        button.classList.add('active')
+      }
+
+      function showRawFrames(button) {
+        const parent = button.closest('section')
+
+        const formattedFrames = parent.querySelector('#stack-frames-formatted')
+        formattedFrames.classList.remove('visible')
+
+        const rawFrames = parent.querySelector('#stack-frames-raw')
+        rawFrames.classList.add('visible')
+
+        button.parentElement.querySelectorAll('button').forEach((btn) => btn.classList.remove('active'))
+        button.classList.add('active')
+      }
+
+      function toggleFrameSource(parent) {
+        if (parent.classList.contains('expanded')) {
+          parent.classList.remove('expanded')
+        } else {
+          parent.classList.add('expanded')
+        }
+      }
+
+      window.addEventListener('DOMContentLoaded', () => {
+        document.querySelector('#formatted-frames-toggle').addEventListener('click', function () {
+          showFormattedFrames(this)
+        })
+        document.querySelector('#raw-frames-toggle').addEventListener('click', function () {
+          showRawFrames(this)
+        })
+
+        document.querySelectorAll('button[class=\\"stack-frame-location\\"]').forEach((sfl) => {
+          sfl.addEventListener('click', function (e) {
+            if (e.target.tagName === 'A') {
+              return
+            }
+            toggleFrameSource(e.target.closest('li'))
+          })
+        })
+
+        document.querySelectorAll('button[class=\\"stack-frame-toggle-indicator\\"]').forEach((sfl) => {
+          sfl.addEventListener('click', function (e) {
+            toggleFrameSource(e.target.closest('li'))
+          })
+        })
+      })"
+    `)
     expect(window.document.querySelector('#error-hint')).toEqual(null)
   })
 
