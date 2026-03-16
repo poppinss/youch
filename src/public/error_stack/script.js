2491508150ea47167114981023f98db88e60811f
Agustin Coelho <77288330+aguscoe@users.noreply.github.com>
2026-03-16 12:51:23 -0300

    Add optional chaining

--- src/public/error_stack/script.js
+++ src/public/error_stack/script.js
@@ -44,15 +44,15 @@ function toggleAllFrames() {
   }
 }
 
-document.querySelector('#formatted-frames-toggle').addEventListener('click', function () {
+document.querySelector('#formatted-frames-toggle')?.addEventListener('click', function () {
   showFormattedFrames(this)
 })
-document.querySelector('#raw-frames-toggle').addEventListener('click', function () {
+document.querySelector('#raw-frames-toggle')?.addEventListener('click', function () {
   showRawFrames(this)
 })
 document
   .querySelector('#all-frames-toggle input[type="checkbox"]')
-  .addEventListener('change', function () {
+  ?.addEventListener('change', function () {
     toggleAllFrames()
   })
 
