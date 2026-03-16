79e0f05c09d0f0fb2921309d6da1d60396105e72
Agustin Coelho <77288330+aguscoe@users.noreply.github.com>
2026-03-16 13:14:10 -0300

    Add specs

--- tests/templates.spec.ts
+++ tests/templates.spec.ts
@@ -96,15 +96,15 @@ test.group('Templates', () => {
         }
       }
 
-      document.querySelector('#formatted-frames-toggle').addEventListener('click', function () {
+      document.querySelector('#formatted-frames-toggle')?.addEventListener('click', function () {
         showFormattedFrames(this)
       })
-      document.querySelector('#raw-frames-toggle').addEventListener('click', function () {
+      document.querySelector('#raw-frames-toggle')?.addEventListener('click', function () {
         showRawFrames(this)
       })
       document
         .querySelector('#all-frames-toggle input[type=\\"checkbox\\"]')
-        .addEventListener('change', function () {
+        ?.addEventListener('change', function () {
           toggleAllFrames()
         })
 
